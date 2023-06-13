8a0d3769ea7f8b6d185c850bb5eacdeb33e9a589
ryserac <ryserac@mail.uc.edu>
2023-06-12 23:58:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int WRISTBAND_AGE = 21;
+        Scanner in = new Scanner(System.in);
+        int age = 2;
+        if (age >= WRISTBAND_AGE) {
+            System.out.println("You get a wristband");
+        }
+        else {
+        }
+    }
+}
